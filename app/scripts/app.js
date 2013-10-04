8702407e6c5f2a8f8d24c06af87cc17d3adba056
ryan.johnson <ryan.johnson@nurun.com>
2013-10-04 13:33:18 -0400

    test push to see if this shit works

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('nCommerceApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
