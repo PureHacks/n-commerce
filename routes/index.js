93786acecd7ff724e1bdc34c4e41e99165ed642d
Josh Arndt <josh.arndt@nurun.com>
2013-10-04 23:05:44 -0400

    Updated basic architecture: Handlebars, layouts, templates, jQuery, and basic JSON rendering

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,21 @@
 /*
  * GET home page.
  */
+var products = require('./products');
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+
+	//set cookie - http://expressjs.com/api.html#res.cookie
+	res.cookie('test', 'cookie value josh', {signed: true});
+	
+	//var entry = blogEngine.getBlogEntry(req.params.id);
+	var product = products.getProduct(3);
+	//var productResults = products.getProduct();
+
+	res.render('home',{
+		title:'N-Commerce Home'
+		, productName: product.name
+		, productDesc: product.desc
+		, productList:products.getProducts()
+	});
 };
