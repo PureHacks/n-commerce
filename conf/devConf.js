1fdfeeebd45b60418a0382782794e2a7fd53040a
Alin <dorudumi@gmail.com>
2013-10-11 02:59:16 -0400

    Add configuration for PayPal payments.

--- conf/devConf.js
+++ conf/devConf.js
@@ -6,7 +6,16 @@
 
 var devConf = {
   databaseName: 'test',
-  databaseHost: 'localhost'
+  databaseHost: 'localhost',
+  payPal: {
+    "port" : 5000,
+    "api" : {
+      "host" : "api.sandbox.paypal.com",
+      "port" : "",
+      "client_id" : "ARtpgRB02F650dhBMrZ1u-9G5GMkEJwc8hVlU8Mt85RkIvewNAu1rXkHTi8L",
+      "client_secret" : "EKItsRCjlq3Rx-q7eoHp8cCefbT8dY635Gn0AGQ0pjju04gy7KHt_jK1N2aC"
+    }
+  }
 };
 
 exports.conf = devConf;
